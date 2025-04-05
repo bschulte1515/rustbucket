9f3b934a8381217654cde2f5f2fa01a1a91fd893
lecluyse2000 <lecluyse20@gmail.com>
2025-04-05 01:28:59 -0500

    command life stuff

--- rustbucket/src/lib.rs
+++ rustbucket/src/lib.rs
@@ -1,26 +1,28 @@
 mod tools;
+use tools::Tool;
 
-pub struct Config {
-    tool: tools::Tool,
-}
-
-impl Config {
-    pub fn build(mut args: impl Iterator<Item = String>) -> None { // -> Result<Config, &'static str) {
-        args.next(); // Skip name of the program 
-        while let Some(arg) = args.next() {
-            match arg.as_str() {
-                "-t" => {
-                    if let Some(value) = arg.next() {
-                        println!("Using tool {value}");
-                        return Config(  )
-                    } else {
-                        println!("Did not supply a tool name");
-                    }
-                }
-                _ => println!("Invalid flag: {arg}");
-            }
-        }
+fn check_tool(&input: str) -> Tool {
+    match value.as_str() {
+        "keylogger" => return Tool::Keylogger,
+        "clipboard" => return Tool::Clipboard,
+        _ => return Tool::Invalid, 
     }
 }
 
-pub fn run() -> 
+pub fn build(mut args: impl Iterator<Item = String>) -> Result<Tool, &'static str> {
+    args.next(); 
+    while let Some(arg) = args.next() {
+        match arg.as_str() {
+            "-t" | "--tool" => {
+                if let Some(value) = args.next() {
+                    return Ok(check_tool(value));
+                } else {
+                    return Err("Did not supply a tool name");
+                }
+            }
+            _ => return Err("Invalid flag: {arg}"),
+        }
+    }
+    
+    Err("No tool specified. Use -t or --tool to specify a tool.")
+}
